232458ca7cf5bd8408cc83c96272eda14df97609
digis <digis@ALIEN10>
2023-12-12 14:06:29 -0600

    Inicialización de proyecto

---
+++ src/main/java/com/digipro/Equipo3DP/Equipo3DpApplication.java
@@ -0,0 +1,13 @@
+package com.digipro.Equipo3DP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Equipo3DpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Equipo3DpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digipro/Equipo3DP/Equipo3DpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digipro.Equipo3DP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Equipo3DpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
