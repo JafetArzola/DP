321621715396261218bc559878ea4ea6c60b4c4d
ARoblesD06 <147754192+ARoblesD06@users.noreply.github.com>
2023-12-13 16:34:32 -0600

    Update AlumnoRepository.java

--- src/main/java/com/digipro/Equipo3DP/DL/AlumnoRepository.java
+++ src/main/java/com/digipro/Equipo3DP/DL/AlumnoRepository.java
@@ -11,4 +11,34 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author digis
  */
 public interface AlumnoRepository extends JpaRepository<Alumno, Integer> {
+@Procedure(name = "AddAlumnoSP")
+    void addAlumnoSP(
+            @Param("p_nombre") String nombre,
+            @Param("p_apellido_paterno") String apellidoPaterno,
+            @Param("p_apellido_materno") String apellidoMaterno
+    );
+    
+     @Procedure(name = "UpdateAlumnoSP")
+    void updateAlumnoSP(
+        @Param("p_id_alumno") Long idAlumno,
+        @Param("p_nombre") String nombre,
+        @Param("p_apellido_paterno") String apellidoPaterno,
+        @Param("p_apellido_materno") String apellidoMaterno
+    );
+    
+    @Procedure(name = "DeleteAlumnoSP")
+    void deleteAlumnoSP(@Param("p_id_alumno") Long idAlumno);
+    
+    @Query(nativeQuery = true, value = "{call GetAllAlumnoSP(:p_cursor)}")
+    List<Alumno> getAllAlumnosSP(@Param("p_cursor") List<Object[]> p_cursor);
+    
+    @Query(nativeQuery = true, value = "{call GetByIdAlumnoSP(:p_id_alumno, :p_cursor)}")
+    List<Alumno> getByIdAlumnoSP(
+        @Param("p_id_alumno") Long idAlumno,
+        @Param("p_cursor") List<Object[]> p_cursor
+    );
+
+  
 }
+
+
