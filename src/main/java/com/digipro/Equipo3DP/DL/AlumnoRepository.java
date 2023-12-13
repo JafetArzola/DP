433b85556fb9c9170a6fbd5f8a676024e2952f79
digis <digis@ALIEN10>
2023-12-13 16:56:23 -0600

    Verificación de pruebas con SP mapeados

--- src/main/java/com/digipro/Equipo3DP/DL/AlumnoRepository.java
+++ src/main/java/com/digipro/Equipo3DP/DL/AlumnoRepository.java
@@ -4,7 +4,11 @@
  */
 package com.digipro.Equipo3DP.DL;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 
 /**
  *
