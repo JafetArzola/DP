6ddc7830f1b512904540338469e062c31b60563b
ARoblesD06 <147754192+ARoblesD06@users.noreply.github.com>
2023-12-13 18:33:42 -0600

    Update MateriaRepository.java

--- src/main/java/com/digipro/Equipo3DP/DL/MateriaRepository.java
+++ src/main/java/com/digipro/Equipo3DP/DL/MateriaRepository.java
@@ -11,5 +11,25 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author digis
  */
 public interface MateriaRepository extends JpaRepository<Materia, Integer>{
-    
+    @Procedure(name = "AddMateriaSP")
+    void addMateriaSP(
+            @Param("p_nombre") String nombre,
+            @Param("p_costo") Double costo
+    );
+
+    @Procedure(name = "UpdateMateriaSP")
+    void updateMateriaSP(
+            @Param("p_id_materia") int idMateria,
+            @Param("p_nombre") String nombre,
+            @Param("p_costo") Double costo
+    );
+
+    @Procedure(name = "DeleteMateriaSP")
+    void deleteMateriaSP(@Param("p_id_materia") int idMateria);
+
+    @Procedure(name = "GetAllMateriaSP", outputParameterName = "p_cursor")
+    void getAllMateriaSP();
+
+    @Procedure(name = "GetByIdMateriaSP", outputParameterName = "p_cursor")
+    void getByIdMateriaSP(@Param("p_id_materia") int idMateria);
 }
