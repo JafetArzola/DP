c7359a2004cefdb3ab9d8476c8357c1a26d01738
digis <digis@ALIEN10>
2023-12-13 18:11:45 -0600

    Se corrigen errores de mapeo de clase con SP

--- src/main/java/com/digipro/Equipo3DP/DL/AlumnoRepository.java
+++ src/main/java/com/digipro/Equipo3DP/DL/AlumnoRepository.java
@@ -4,7 +4,10 @@
  */
 package com.digipro.Equipo3DP.DL;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 
 /**
  *
